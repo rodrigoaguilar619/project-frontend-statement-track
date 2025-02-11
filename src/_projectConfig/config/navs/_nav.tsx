fea07c93b586574464ce569a6786699b7cdddee6
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-02-10 23:28:22 -0600

    Adjust nav format to generic menu format

--- src/_projectConfig/config/navs/_nav.tsx
+++ src/_projectConfig/config/navs/_nav.tsx
@@ -1,43 +1,42 @@
 import { ROUTE_ACCOUNT_STATEMENT_FILTER_GET, ROUTE_ACCOUNT_STATEMENT_SNOWBALL_FILE_LOAD, ROUTE_BROKER_ACCOUNT_LIST_GET, ROUTE_ISSUES_LIST_GET } from '@app/catalogs/routesCatalog'
-import {
-  cilBuilding,
-  cilFile,
-  cilList
-} from '@coreui/icons'
-import CIcon from '@coreui/icons-react'
-import { CNavItem, CNavTitle } from '@coreui/react'
-import { faFilter } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { AppMenusPropsDataI } from 'lib-components-react/lib/@types/components/layout/appMenuLayout'
+import { ROUTE_LOGOUT } from 'lib-components-react/lib/catalogs/routesCatalog'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBuilding, faExternalLink, faFile, faFilter, faList, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { PATH_API_DOCUMENTATION } from '@app/catalogs/uriCatalog';
 
-const _nav = [
-  {
-    component: CNavTitle,
-    name: 'Modules',
-  },
-  {
-    component: CNavItem,
-    name: 'Issues Catalog',
-    to: ROUTE_ISSUES_LIST_GET,
-    icon: <CIcon icon={cilList} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Broker Accounts',
-    to: ROUTE_BROKER_ACCOUNT_LIST_GET,
-    icon: <CIcon icon={cilBuilding} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Broker Accounts Filter',
-    to: ROUTE_ACCOUNT_STATEMENT_FILTER_GET,
-    icon: <FontAwesomeIcon icon={faFilter} className='nav-icon' />,
-  },
-  {
-    component: CNavItem,
-    name: 'Account Statement',
-    to: ROUTE_ACCOUNT_STATEMENT_SNOWBALL_FILE_LOAD,
-    icon: <CIcon icon={cilFile} customClassName="nav-icon" />,
-  }
-]
+const _nav: AppMenusPropsDataI[] = [
+    {
+      text: 'Issues Catalog',
+      url: ROUTE_ISSUES_LIST_GET,
+      icon: <FontAwesomeIcon icon={faList} className="nav-icon menu-icon" />,
+    },
+    {
+      text: 'Broker Accounts',
+      url: ROUTE_BROKER_ACCOUNT_LIST_GET,
+      icon: <FontAwesomeIcon icon={faBuilding} className="nav-icon menu-icon" />,
+    },
+    {
+      text: 'Broker Accounts Filter',
+      url: ROUTE_ACCOUNT_STATEMENT_FILTER_GET,
+      icon: <FontAwesomeIcon icon={faFilter} className="nav-icon menu-icon" />,
+    },
+    {
+      text: 'Account Statement',
+      url: ROUTE_ACCOUNT_STATEMENT_SNOWBALL_FILE_LOAD,
+      icon: <FontAwesomeIcon icon={faFile} className="nav-icon menu-icon" />,
+    },
+    {
+      text: 'Documentation API',
+      url: PATH_API_DOCUMENTATION,
+      icon: <FontAwesomeIcon icon={faExternalLink} className="nav-icon menu-icon" />,
+      isOpenExternal: true
+    },
+    {
+      text: 'Logout',
+      url: ROUTE_LOGOUT,
+      icon: <FontAwesomeIcon icon={faRightFromBracket} className="nav-icon menu-icon" />
+    }
+];
 
-export default _nav
+export default _nav;
